3b5240d7d608b7a33f72e3951a251f6e78752125
Parth Garg <parthgarg243@gmail.com>
2023-11-15 20:50:06 +0530

    minor

--- src/main/java/com/example/stickhero/Main.java
+++ src/main/java/com/example/stickhero/Main.java
@@ -2,22 +2,44 @@ package com.example.stickhero;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group root = new Group();
+//        Image icon = new Image("logo.png");
+        Scene scene = new Scene(root, Color.BLACK);
+//        stage.getIcons().add(icon);
+        stage.setTitle("Stick Hero");
         stage.setScene(scene);
+        stage.setHeight(716);
+        stage.setWidth(500);
+        stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch();
+//    }
 }
