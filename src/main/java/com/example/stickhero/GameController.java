37c6ff3535f6990e8d353f1b88aa15a985c0ced1
Parth Garg <parthgarg243@gmail.com>
2023-11-29 02:27:29 +0530

    minor

--- src/main/java/com/example/stickhero/GameController.java
+++ src/main/java/com/example/stickhero/GameController.java
@@ -1,5 +1,6 @@
 package com.example.stickhero;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,26 +9,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.Node;
-import javafx.scene.Parent;
+
 public class GameController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private Hero myHero = new Hero(60, 45, 25, 400);
+    private Platform currentPlatform = new Platform();
 
     public void switchToStartScreen(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
