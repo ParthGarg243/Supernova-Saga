8c71ec51d0595c47a9a13bb5cfea32a448aa0e1a
Parth Garg <parthgarg243@gmail.com>
2023-11-29 18:38:48 +0530

    something

--- src/main/java/com/example/stickhero/GameController.java
+++ src/main/java/com/example/stickhero/GameController.java
@@ -1,83 +1,7 @@
-//package com.example.stickhero;
-//
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Node;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.scene.image.ImageView;
-//import javafx.scene.input.KeyCode;
-//import javafx.scene.input.KeyEvent;
-//import javafx.scene.layout.AnchorPane;
-//import javafx.scene.shape.Line;
-//import javafx.scene.shape.Rectangle;
-//import javafx.stage.Stage;
-//
-//import java.io.IOException;
-//
-//public class GameController {
-//    private Stage stage;
-//    private Scene scene;
-//    private Parent root;
-//
-//    @FXML
-//    private ImageView heroImage;
-//    @FXML
-//    private Rectangle currentPlatform;
-//    @FXML
-//    private Rectangle nextPlatform;
-//    @FXML
-//    private Line stick;
-//    @FXML
-//    private AnchorPane gamePane;
-//
-//    public void switchToStartScreen(ActionEvent event) throws IOException {
-//        root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    public void switchToGameScreen(ActionEvent event) throws IOException {
-//        root = FXMLLoader.load(getClass().getResource("GameplayScreen.fxml"));
-//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-////    @FXML
-////    public void keyPressed(KeyEvent event) {
-////        if (event.getCode() == KeyCode.SPACE) {
-////            spacebarPressed = true;
-////            increaseLength();
-////        }
-////    }
-////
-////    @FXML
-////    public void keyReleased(KeyEvent event) {
-////        if (event.getCode() == KeyCode.SPACE) {
-////            spacebarPressed = false;
-////        }
-////    }
-//
-//    public void increaseLength() {
-//        // Increase the stick length by adjusting the endY coordinate
-//        double newStickLength = stick.getEndY() - 25.0;  // Adjust the increment as needed
-//        stick.setEndY(newStickLength);
-//    }
-//
-//    public void setMainWindow(Stage stage) {
-//        this.stage = stage;
-//    }
-//}
-
 package com.example.stickhero;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -85,8 +9,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
@@ -113,9 +35,9 @@ public class GameController {
     private AnchorPane gamePane;
 
     private long startTime;
-    private Timeline decreaseTimeline;
-    private static final double INCREASE_AMOUNT = 10.0;
-    private static final Duration INCREASE_INTERVAL = Duration.millis(200);
+    private static final double INCREASE_AMOUNT = 1.0;
+    private static final long INCREASE_INTERVAL = 25; // milliseconds
+    private int isMousePressed = 0;
 
     public void switchToStartScreen(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
@@ -126,46 +48,104 @@ public class GameController {
     }
 
     public void switchToGameScreen(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("GameplayScreen.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GameplayScreen.fxml"));
+        Parent root = loader.load();
+        GameController controller = loader.getController();
+
+        // Set the controller for FXMLLoader explicitly
+        loader.setController(controller);
+
+        // Rest of your code remains the same
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
 
         // Set up mouse pressed and released event handlers
-        gamePane.setOnMousePressed(this::handleMousePressed);
-        gamePane.setOnMouseReleased(this::handleMouseReleased);
+        controller.setMainWindow(stage); // Ensure the controller has a reference to the stage
+        controller.initializeGame(); // Call a method to initialize game-related components
 
-        // Initialize the Timeline for decreasing stick length
-        decreaseTimeline = new Timeline(
-                new KeyFrame(INCREASE_INTERVAL, mouseEvent -> increaseLength())
-        );
-        decreaseTimeline.setCycleCount(Timeline.INDEFINITE);
+        // Start the loop in a separate thread
+        new Thread(controller::increaseLengthLoop).start();
     }
 
-    private void handleMousePressed(MouseEvent mouseEvent) {
+
+    public void handleMousePressed(MouseEvent mouseEvent) {
         // Record the start time when the mouse is pressed
         startTime = System.currentTimeMillis();
 
-        // Start the timeline when the mouse is pressed
-        decreaseTimeline.play();
+        // Set isMousePressed to 1
+        isMousePressed = 1;
     }
 
-    private void handleMouseReleased(MouseEvent mouseEvent) {
-        // Stop the timeline when the mouse is released
-        decreaseTimeline.stop();
+    public void handleMouseReleased(MouseEvent mouseEvent) {
+        // Set isMousePressed to 0
+        isMousePressed = 0;
+
+        rotateStickAndMoveHero();
     }
 
-    private void increaseLength() {
-        // Calculate elapsed time since the mouse was pressed
-        long currentTime = System.currentTimeMillis();
-        long elapsedTime = currentTime - startTime;
+    private void increaseLengthLoop() {
+        while (true) {
+            if (isMousePressed == 1) {
+                long currentTime = System.currentTimeMillis();
+                long elapsedTime = currentTime - startTime;
 
-        // Decrease the stick length based on elapsed time
-        double newStickLength = stick.getEndY() - (INCREASE_AMOUNT * (elapsedTime / 200.0));
+                double newStickLength = stick.getEndY() - (INCREASE_AMOUNT * (elapsedTime / 200.0));
 
-        // Update the stick length
-        stick.setEndY(newStickLength);
+                stick.setEndY(newStickLength);
+            }
+
+            try {
+                // Sleep for the specified interval
+                Thread.sleep(INCREASE_INTERVAL);
+            } catch (InterruptedException e) {
+                // Handle interruption if needed
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void rotateStickAndMoveHero() {
+        // Set the pivot point to the bottom end of the line
+        stick.setRotate(0); // Reset rotation
+        stick.setStartY(((-1) * stick.getEndY()));
+        stick.setTranslateY(currentPlatform.getTranslateY() + 1);
+
+        // Create a RotateTransition for the line
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(1), stick);
+
+        // Set the angle of rotation (90 degrees)
+        rotateTransition.setByAngle(90);
+
+        // Set event handler to trigger moveHeroImage after rotation is finished
+        rotateTransition.setOnFinished(event -> moveHeroImage());
+
+        // Play the rotation animation
+        rotateTransition.play();
+    }
+
+    private void moveHeroImage() {
+        // Create a TranslateTransition for heroImage
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(2), heroImage);
+
+        // Set the destination X coordinate (adjust as needed)
+        double destinationX = nextPlatform.getLayoutX() - nextPlatform.getWidth() / 2;
+
+        // Set the destination Y coordinate (same as the current position)
+        double destinationY = heroImage.getTranslateY();
+
+        // Set the new position
+        translateTransition.setToX(destinationX);
+        translateTransition.setToY(destinationY);
+
+        // Play the translation animation
+        translateTransition.play();
+    }
+
+    public void initializeGame() {
+        gamePane.setOnMousePressed(this::handleMousePressed);
+        gamePane.setOnMouseReleased(this::handleMouseReleased);
     }
 
     public void setMainWindow(Stage stage) {
