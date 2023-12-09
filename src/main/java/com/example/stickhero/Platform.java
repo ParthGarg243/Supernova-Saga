382304cd27badd9ce12130ba17081303533a08e5
Parth Garg <115782300+ParthGarg243@users.noreply.github.com>
2023-12-09 05:41:05 +0530

    Update Platform.java

--- src/main/java/com/example/stickhero/Platform.java
+++ src/main/java/com/example/stickhero/Platform.java
@@ -2,6 +2,8 @@ package com.example.stickhero;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Rectangle;
 
+import java.util.Random;
+
 public class Platform extends Rectangle{
     @FXML
     private Rectangle thisPlatform;
@@ -11,6 +13,18 @@ public class Platform extends Rectangle{
     public Platform(float length, float width, float xCoord, float yCoord, Rectangle rectangle) {
         super(length, width, xCoord, yCoord);
         this.thisPlatform = rectangle;
+    }
+
+    public static double generateWidth() {
+        Random random = new Random();
+
+        return random.nextInt(56) + 45;
+    }
+
+    public static double generateLayoutX() {
+        Random random = new Random();
+
+        return random.nextInt(211) + 90;
     }
 
     public Rectangle getThisPlatform() {
